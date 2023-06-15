0f890ad8876049181aac1db8906efe75258f67b6
nigashish <ashishn@eupheus.in>
2023-06-15 18:06:35 +0530

    New service for Jobs

---
+++ src/main/java/com/eupheus/visor/job/api/VisorJobServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eupheus.visor.job.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisorJobServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisorJobServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eupheus/visor/job/api/VisorJobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eupheus.visor.job.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisorJobServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
