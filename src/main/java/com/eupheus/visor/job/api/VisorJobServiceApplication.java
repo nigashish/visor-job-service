cb307a71efeebbd46a54474f65372d233170027a
nigashish <ashishn@eupheus.in>
2023-06-23 13:18:58 +0530

    DB Regarding  changes

--- src/main/java/com/eupheus/visor/job/api/VisorJobServiceApplication.java
+++ src/main/java/com/eupheus/visor/job/api/VisorJobServiceApplication.java
@@ -3,16 +3,25 @@ package com.eupheus.visor.job.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableEurekaClient
 @EntityScan(basePackages = {"com.accelerate.visor"})
 public class VisorJobServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VisorJobServiceApplication.class, args);
 	}
-
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	}
 }
